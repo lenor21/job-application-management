70afc9474a174724441f6836bde9dcfc630654b9
lenor21 <dejesusronel29@gmail.com>
2025-08-13 22:45:49 +0800

    fixed docker for development

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM node:20-alpine
+FROM node:18-alpine
 
 WORKDIR /app
+
 COPY package*.json ./
+
 RUN npm install
 
 COPY . .
+
 EXPOSE 3000
+
 CMD ["npm", "run", "dev"]
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.watchOptions = {
-        poll: 1000, // Check for changes every 1000ms (1 second)
-        aggregateTimeout: 300, // Debounce time for multiple changes
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
